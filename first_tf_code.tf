f013563f0cbca08584b27db13664c2d69bc67e8a
Roger Mbah <roger4mbah@gmail.com>
2022-04-25 01:17:24 -0400

    create new feature branch

--- first_tf_code.tf
+++ first_tf_code.tf
@@ -8,6 +8,11 @@ provider "aws" {
   acl    = "private"
   }
 
+  resource "aws_s3_bucket" "tf_course1" {
+  bucket = "tf-course-202204241"
+  acl    = "private"
+  }
+
 resource "aws_instance" "my_ec2" {
   ami     = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
