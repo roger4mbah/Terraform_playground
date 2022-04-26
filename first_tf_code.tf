e62e65a4619cdf729a22a1e76e0b75d13e831486
Roger Mbah <roger4mbah@gmail.com>
2022-04-26 17:31:46 -0400

    add sg & vpc all existing

--- first_tf_code.tf
+++ first_tf_code.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
   profile = "default"
   region = "us-east-1"
@@ -16,8 +17,19 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami     = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
+  subnet_id = "subnet-0a4a1a488ce334610"
+  associate_public_ip_address = "true"
+  key_name = "demo_key"
+  vpc_security_group_ids = ["sg-0a345d8592fbc038b" ]
   tags = {
      Name = "genius_instance"
  }
+ user_data = <<EOF
+ #!/bin/bash
+ yum update -y
+ yum install httpd
+ systemctl enable --now httpd
+EOF
+ 
 }
 
